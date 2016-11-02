950d42390d9be02f32de77bb5fd5285a98374893
jinalex <jin.alex123@gmail.com>
2016-11-02 10:04:15 +0100

    thank you message

--- js/new-age.js
+++ js/new-age.js
@@ -26,6 +26,63 @@
         offset: {
             top: 50
         }
-    })
+    });
+
+        // Vars.
+            var $form = document.querySelectorAll('#signup-form')[0],
+                $submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+                $message;
+
+        // Bail if addEventListener isn't supported.
+            if (!('addEventListener' in $form))
+                return;
+
+        // Message.
+            $message = document.createElement('span');
+                $message.classList.add('message');
+                $form.appendChild($message);
+
+            $message._show = function(type, text) {
+
+                $message.innerHTML = text;
+                $message.classList.add(type);
+                $message.classList.add('visible');
+
+                window.setTimeout(function() {
+                    $message._hide();
+                }, 3000);
+
+            };
+
+            $message._hide = function() {
+                $message.classList.remove('visible');
+            };
+
+        // Events.
+        // Note: If you're *not* using AJAX, get rid of this event listener.
+            $form.addEventListener('submit', function(event) {
+
+                // Hide message.
+                    $message._hide();
+
+                // Disable submit.
+                    $submit.disabled = true;
+
+                // Process form.
+                    window.setTimeout(function() {
+
+                        // Reset form.
+                            $form.reset();
+
+                        // Enable submit.
+                            $submit.disabled = false;
+
+                        // Show message.
+                            $message._show('success', '<strong>Thank you!</strong>');
+                            //$message._show('failure', 'Something went wrong. Please try again.');
+
+                    }, 750);
+
+            });
 
 })(jQuery); // End of use strict
